79542c4a806e32bcbf71d364f62e053cefac07db
elgac808 <JC60@students.ptcollege.edu>
2020-02-05 14:35:45 -0500

    forCotzclass

---
+++ blag/src/blag/Blag.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blag;
+
+/**
+ *
+ * @author cagle
+ */
+public class Blag {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+          cagle();
+    }
+    
+    public static void cagle(){
+        System.out.println("github woo hoo");
+    
+    }
+}
